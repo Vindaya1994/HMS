fa9b686f79765dbdd82dca14cfc7c12c1a551d82
Vindaya Fernando <vindayatechno@gmail.com>
2018-10-30 15:14:19 +0530

    Add files via upload

---
+++ footers/footer4.php
@@ -0,0 +1,59 @@
+			<div class="section-footer-content row">
+				<div class="footer-menu-wrapper col-sm-5 col-sm-push-1 col-md-7 col-lg-5 col-lg-push-4">
+					<div class="inner">
+						<ul class="footer-menu">
+                            <li><a href="">Customer</a></li>
+							<li><a href="">Rooms</a></li>
+							<li><a href="">Dining</a></li>
+							<li><a href="">Services</a></li>
+							 
+						</ul>
+						<ul class="footer-menu">
+							<li><a href="">Promotions</a></li>
+							<li><a href="">Job Vacancies</a></li>
+							<li><a href="">Feedback</a>  </li>
+							<li><a href="">Inventory</a>  </li>     
+						</ul>
+					</div>
+					<div class="inner">
+						<ul class="footer-menu">
+                            <li><a href="">Staff</a>  </li> 
+							<li><a href="about_us.php">About us</a> </li>
+							<li><a href="contact_us.php">Contact us</a></li>
+						</ul>
+					</div>
+					
+				</div>
+				<ul class="footer-contact-wrapper col-sm-5 col-sm-push-1 col-md-4 col-lg-3 col-lg-pull-4 col-lg-push-0">
+					<li><span class="label">Address:</span><span >Main Road, Bandarawella, Sri Lanka.</span></li>
+					<li><span class="label">Email:</span><span ><a href="">Tenthhotel@mail.com</a></span></li>
+					<li><span class="label">Phone:</span><span ><a href="tel:+94222222222">+94 222 222 222</a></span></li>
+				</ul>
+			</div>
+			<div class="section-footer-bottom row">
+				<ul class="social-media-wrapper col-xs-12 ">
+					<li>
+						<a href="" target="_blank">
+							<i class="fa fa-facebook-official" style="font-size:24px"></i>
+						</a>
+					</li>
+					<li>
+						<a href="" target="_blank">fa fa-instagram
+							<i class="fa fa-instagram" style="font-size:24px"></i>
+						</a>
+					</li>
+					<li>
+						<a href="" target="_blank">
+							<i class="fa fa-twitter-square" style="font-size:24px"></i>
+						</a>
+					</li>
+				</ul>
+				<div class="bottom-content-wrapper">
+					<div class="col-xs-12 col-lg-6"style="text-align:right"><span class="copyright">All copyrights reserved by</span> The Tenth Hotel - © 2018  |</div>
+					<div class="col-xs-12 col-lg-6"style="text-align:left"><span><a href="index.php">Sitemap | </a> </span>
+						  <span><a href="#"> Privacy Policy | </a></span>
+						  <span><a href="#"> Terms of Use</a></span>
+					</div>
+					<!--<div class="col-xs-12 col-lg-4">Website Designed &amp; Developed by <a href="#" target="_blank" class="eme-link"></a></div>-->
+				</div>
+			</div>	
